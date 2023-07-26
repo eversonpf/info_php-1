f396489aa1589d4f655c44718e60dc2efea235a9
Eduardo Passini <138033841+eduardopassini@users.noreply.github.com>
2023-07-25 21:59:21 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -25,3 +25,38 @@ if ($operador == '/'){
 echo $resultado;
 
 
+<?php
+
+$value = '2';
+$value2 = '5';
+$operador = '%';
+$resultado = '0';
+
+
+if ($operador == '+'){
+    $resultado = $value + $value2;
+}
+
+if ($operador == '-'){
+    $resultado = $value - $value2;
+}
+
+if ($operador == '*'){
+    $resultado = $value * $value2;
+}
+
+if ($operador == '/'){
+    $resultado = $value / $value2;
+}
+
+if ($operador == '^'){
+    $resultado = pow($value, $value2);
+    $resultado = $value ** $value2;
+}
+
+if ($operador == '%'){
+    $resultado = pow($value, $value2);
+    $resultado = $value % $value2;
+}
+
+echo "o valor é {$resultado}";
