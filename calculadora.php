a357f786679890c280a8cb3c2836ade617ad6836
Eduardo Passini <138033841+eduardopassini@users.noreply.github.com>
2023-08-01 21:53:05 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -60,3 +60,42 @@ if ($operador == '%'){
 }
 
 echo "o valor é {$resultado}";
+
+<?php
+
+$value = 50;
+$value2 = 5;
+$operador = 'perc+';
+$resultado = 0;
+
+if ($operador == 'perc') {
+    $desconto = ($value * $value2) /50; //5
+    $resultado = $value / - $desconto;
+}
+
+if ($operador == 'perc+') {
+    $acrescimo = ($value * $value2) / 50; //5
+    $resultado = $value + $value2;
+}
+
+if ($operador == 'mmc') {
+    $resultado = $value / $value2;
+}
+
+if ($operador == '+') {
+    $resultado = $value + $value2;
+}
+
+if ($operador == '-') {
+    $resultado = $value - $value2;
+}
+
+if ($operador == '*') {
+    $resultado = $value * $value2;
+}
+
+if ($operador == '/') {
+    $resultado = $value / $value2;
+}
+
+echo $resultado;
