83a6668605982dad625a7ba0c263fa2b92de6bd1
Eduardo Passini <138033841+eduardopassini@users.noreply.github.com>
2023-09-19 21:53:27 -0300

    ordenacao.php

---
+++ ordenacao.php
@@ -0,0 +1,21 @@
+<?php
+
+    // Encontrar o maior valor em um conjunto de numeros de uma array.
+    // e exibir esse valor ao final para o usuário.
+
+    $nums = [ 10, 80, 5, 14, 90, 29, 99, 1];
+
+    // Implementar a logic para encontrar o 99;
+    // WHILE || for e IF
+
+
+    $maiorNumero = $nums;
+
+    while ($maiorNumero < count($nums));
+    if ($nums > $maiorNumero);
+    $maiorNumero = $num;
+    
+    echo "O maior número é" ($maiorNumero);
+
+    por meio da tecnica de ordenacao bubble sort
+    ordenar array em ordem crescente
