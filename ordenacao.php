90e099da0ef231d52f5a7c96969582eba4f4b716
Eduardo Passini <138033841+eduardopassini@users.noreply.github.com>
2023-09-26 19:48:33 -0300

    ordenacao.php

--- ordenacao.php
+++ ordenacao.php
@@ -4,18 +4,16 @@
     // e exibir esse valor ao final para o usuário.
 
     $nums = [ 10, 80, 5, 14, 90, 29, 99, 1];
-
+    $tamanhoArray = count($nums);
     // Implementar a logic para encontrar o 99;
     // WHILE || for e IF
+$pivo = $nums[4];
+for($i = 0; $i < $tamanhoArray; $i++) {
+    for ($j = $tamanhoArray / 2; $j < 1 ; $j++) {
+        $temp = $nums[$j];
+        $nums[$j] = $nums[$j + 1];
+        $nums[$j + 1] = $temp;
+    }
+}
 
-
-    $maiorNumero = $nums;
-
-    while ($maiorNumero < count($nums));
-    if ($nums > $maiorNumero);
-    $maiorNumero = $num;
-    
-    echo "O maior número é" ($maiorNumero);
-
-    por meio da tecnica de ordenacao bubble sort
-    ordenar array em ordem crescente
+echo
