bc6982831040f15a6303aec4f61564d4a2e31fc7
Everson de Freitas <137961764+eversonpf@users.noreply.github.com>
2023-09-26 21:53:27 -0300

    Update ordenacao.php

--- ordenacao.php
+++ ordenacao.php
@@ -17,3 +17,48 @@ for($i = 0; $i < $tamanhoArray; $i++) {
 }
 
 echo
+    <?php
+
+    // Encontrar o maior valor em um conjunto de numeros de uma array.
+    // e exibir esse valor ao final para o usuário.
+
+    $nums = [ 10, 80, 5, 14, 90, 29, 99, 1];
+    $tamanhoArray = count($nums);
+    // Implementar a logic para encontrar o 99;
+    // WHILE || for e IF
+$pivo = $nums[4];
+for($i = 0; $i < $tamanhoArray; $i++) {
+    for ($j = $tamanhoArray / 2; $j < 1 ; $j++) {
+        $temp = $nums[$j];
+        $nums[$j] = $nums[$j + 1];
+        $nums[$j + 1] = $temp;
+    }
+}
+
+echo
+
+// Remover valores duplicados de um array.
+$frutas = [
+    "maça",
+    "banana",
+    "laranja",
+    "banana",
+    "uva",
+    "maça","maça",
+];
+  // Saida esperada :maça, banana , laranja, uva
+  // Dica: in_array(), array_merge(), isset(), array_push()   
+
+  $newFrutas = [];
+  for($i =; $i < count($frutas); $i++) {
+
+      $fruta = $frutas[$i];
+     
+      if (!isset(newFrutas[$fruta])){
+        $newFrutas[$fruta] = 1; // ["maca"] = 1
+      } else {
+        $newFrutas[$fruta] += 1; // [ "maca"] += 1(2)
+
+      }
+    }
+    echo implode("," $newFrutas,$frutas,);
